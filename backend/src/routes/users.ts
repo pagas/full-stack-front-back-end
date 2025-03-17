05c2c6fb960b32b8c50e3d1cd1e9a149da192931
Paulius G <paulius@2simple.com>
2025-03-17 23:03:50 +0000

    feat: add user login route to authenticate users and return JWT token

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,4 +1,4 @@
-import { createUser } from '../services/users.js'
+import { createUser, loginUser } from '../services/users.js'
 import { Express } from 'express'
 
 export function userRoutes(app: Express): void {
@@ -10,6 +10,17 @@ export function userRoutes(app: Express): void {
       console.error('error creating user', err)
       return res.status(400).json({
         error: 'failed to create the user, does the username already exist?',
+      })
+    }
+  })
+  app.post('/api/v1/user/login', async (req, res) => {
+    try {
+      const token = await loginUser(req.body)
+      return res.status(200).send({ token })
+    } catch (err) {
+      console.error('error logging in user', err)
+      return res.status(400).send({
+        error: 'login failed, did you enter the correct username/password?',
       })
     }
   })
