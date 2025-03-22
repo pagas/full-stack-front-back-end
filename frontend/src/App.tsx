52f05150cc38767236884e9cb9073de387d23f1f
Paulius G <paulius@2simple.com>
2025-03-22 23:55:28 +0000

    feat: implement routing with react-router-dom and create Blog page with post management features

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,21 @@
 import { JSX } from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { Blog } from './Blog.jsx'
+import { Blog } from './pages/Blog.jsx'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Blog />,
+  },
+])
 
 const queryClient = new QueryClient()
 
 export function App(): JSX.Element {
   return (
     <QueryClientProvider client={queryClient}>
-      <Blog />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   )
 }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import process from 'node:process'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+// Resolve __dirname for ES modules
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig(({ mode }) => {
   // Load environment variables
@@ -10,6 +15,12 @@ export default defineConfig(({ mode }) => {
     plugins: [react()],
     server: {
       port: Number(env.VITE_PORT) || 3000, // Use VITE_PORT or default to 3000
+    },
+    resolve: {
+      alias: {
+        '@components': path.resolve(__dirname, 'src/components'), // Alias for components
+        '@api': path.resolve(__dirname, 'src/api'), // Alias for components
+      },
     },
   }
 })
