b03c8230259107c324ffbce81b4d830111c28bd5
Paulius G <paulius@2simple.com>
2025-03-23 18:55:16 +0000

    feat: add jwt-decode dependency and implement user token display in Header component

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,22 @@
 import { Link } from 'react-router-dom'
 import { JSX } from 'react'
+import { jwtDecode } from 'jwt-decode'
+import { useAuth } from '../contexts/AuthContext.jsx'
 
 export function Header(): JSX.Element {
+  const [token, setToken] = useAuth()
+
+  if (token) {
+    const { sub } = jwtDecode(token)
+    return (
+      <div>
+        Logged in as <b>{sub}</b>
+        <br />
+        <button onClick={() => setToken(null)}>Logout</button>
+      </div>
+    )
+  }
+
   return (
     <div>
       <Link to='/login'>Log In</Link> | <Link to='/signup'>Sign Up</Link>{' '}
